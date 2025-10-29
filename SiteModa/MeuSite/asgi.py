b68170ec3acc19d25b53dbc2c6d542f81c787f81
Caio Romero <caio.romero.tjii@gmail.com>
2025-10-29 23:57:01 +0000

    renamed:    SiteModa/SiteModa/__init__.py -> SiteModa/MeuSite/__init__.py renamed:    SiteModa/SiteModa/asgi.py -> SiteModa/MeuSite/asgi.py renamed:    SiteModa/SiteModa/settings.py -> SiteModa/MeuSite/settings.py renamed:    SiteModa/SiteModa/urls.py -> SiteModa/MeuSite/urls.py renamed:    SiteModa/SiteModa/wsgi.py -> SiteModa/MeuSite/wsgi.py modified:   SiteModa/manage.py

--- SiteModa/MeuSite/asgi.py
+++ SiteModa/MeuSite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SiteModa project.
+ASGI config for MeuSite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SiteModa.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MeuSite.settings")
 
 application = get_asgi_application()
