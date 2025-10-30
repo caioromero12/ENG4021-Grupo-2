3dfc57c4534cf4d3b0c997284f30f3431fab5d64
Caio Romero <caio.romero.tjii@gmail.com>
2025-10-30 01:03:15 +0000

    renamed:    SiteModa/MeuSite/__init__.py -> SiteModa/SiteModa/__init__.py renamed:    SiteModa/MeuSite/asgi.py -> SiteModa/SiteModa/asgi.py renamed:    SiteModa/MeuSite/settings.py -> SiteModa/SiteModa/settings.py renamed:    SiteModa/MeuSite/urls.py -> SiteModa/SiteModa/urls.py renamed:    SiteModa/MeuSite/wsgi.py -> SiteModa/SiteModa/wsgi.py modified:   SiteModa/manage.py new file:   SiteModa/static/css/comentarios.css new file:   SiteModa/static/css/estilos.css new file:   SiteModa/static/css/home.css new file:   SiteModa/static/css/login.css new file:   SiteModa/static/css/moda_sociedade.css new file:   SiteModa/static/css/quemsomos.css new file:   SiteModa/templates/base.html new file:   SiteModa/templates/comentarios.html new file:   SiteModa/templates/estilos.html new file:   SiteModa/templates/index.html new file:   SiteModa/templates/login.html new file:   SiteModa/templates/moda_sociedade.html new file:   SiteModa/templates/quemsomos.html new file:   SiteModa/templates/registration/login.html new file:   SiteModa/usuario/__init__.py new file:   SiteModa/usuario/admin.py new file:   SiteModa/usuario/apps.py new file:   SiteModa/usuario/migrations/__init__.py new file:   SiteModa/usuario/models.py new file:   SiteModa/usuario/tests.py new file:   SiteModa/usuario/urls.py new file:   SiteModa/usuario/views.py

--- SiteModa/SiteModa/wsgi.py
+++ SiteModa/SiteModa/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MeuSite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SiteModa.settings")
 
 application = get_wsgi_application()
